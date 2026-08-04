1cbee2f4c88d26ff71cf64cacaa6583c30975c8c
Shanmuga Krishnan S M <shanmugakrishnan.240293@aids.ritchennai.edu.in>
2026-08-04 14:35:46 +0530

    remove(driver-app): remove rit-driver-app from git repository and add to .gitignore

--- rit-driver-app/app/src/main/java/com/rit/driver/Config.java
+++
@@ -1,9 +0,0 @@
-package com.rit.driver;
-
-public class Config {
-    
-    public static final String BACKEND_URL = "https://rit-services.in";
-    
-   
-    public static final String DEFAULT_PIN = "RITDRIVER";
-}
